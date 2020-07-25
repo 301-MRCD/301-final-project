129aa0685b01eb1c391427eecd635a5159b0a400
Chris-Bortel <bortelcc19@gmail.com>
2020-07-25 16:51:50 -0700

    Built rating average logic

--- schema.sql
+++ schema.sql
@@ -2,7 +2,8 @@ DROP TABLE IF EXISTS parks_table;
 
 CREATE TABLE parks_table(
     id SERIAL PRIMARY KEY,
-    yelp_id VARCHAR (255),
-    total_ratings VARCHAR (255),
+    yelp_id VARCHAR(255),
+    park_name VARCHAR(255),
+    total_ratings VARCHAR(255),
     total_votes VARCHAR(255)
-);
+)
--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 const PORT = process.env.PORT;
 const override = require('method-override');
 
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 app.use(cors());
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: true }));
@@ -37,12 +37,14 @@ app.use(handleError);
 // ROUTE HANDLER FUNCTIONS
 // ----------------------------------------------
 
-function handleHome(req,res) {
-  res.status(200).render('pages/index')
-    .catch(error => handleError(error,res));
+function handleHome(req, res) {
+  res
+    .status(200)
+    .render('pages/index')
+    .catch((error) => handleError(error, res));
 }
 
-function renderResults(req,res) {
+function renderResults(req, res) {
   const searchQuery = req.query.searchQuery;
   const API = `https://api.yelp.com/v3/businesses/search`;
 
@@ -50,90 +52,112 @@ function renderResults(req,res) {
     categories: 'dog_parks',
     sort_by: 'distance',
     location: searchQuery,
-    limit: 5
+    limit: 5,
   };
 
-  superagent.get(API)
+  superagent
+    .get(API)
     .set('Authorization', `Bearer ${process.env.YELP_API_KEY}`)
     .query(queryObject)
-    .then(obj =>{
-      let apiData = obj.body.businesses.map(park => new Park(park));
-      res.status(200).render('pages/results', {parkArr:apiData})
+    .then((obj) => {
+      let apiData = obj.body.businesses.map((park) => new Park(park));
+      res.status(200).render('pages/results', { parkArr: apiData });
     })
 
-    .catch(error => handleError(error,res));
+    .catch((error) => handleError(error, res));
 }
 
-function renderDetail(req,res){
+function renderDetail(req, res) {
   let SQL = `SELECT * FROM parks_table WHERE yelp_id=$1`;
+  // let SQL = `SELECT yelp_id, park_name, sum(total_ratings/total_votes) as avg_rating FROM parks_table WHERE yelp_id=$1 ORDER BY avg DESC;`;
   let values = [req.body.yelp_id];
-  client.query(SQL, values)
-    .then (results => {
-      if (results.rowCount === 0 ){
-        createParkRating(req.body.yelp_id, res)
-        results.status(200).render('pages/detail')
-      }
-      else {
-        // render existing rating 
-        res.status(200).send(results)
-      
-      }
+  client
+    .query(SQL, values)
+    .then((results) => {
+      if (results.rowCount === 0) {
+        console.log(
+          'results from line 74++++++++++++++++++++++++++++++',
+          results
+        );
+        createParkRating(req.body.yelp_id, req.body.name, res);
+      } else {
+        // render existing rating
 
+        let average =
+          results.rows[0].total_ratings / results.rows[0].total_votes || 0;
+        console.log('I am average 94+++++++++++++++++', average);
+        res
+          .status(200)
+          .render('pages/details', {
+            ratings: results.rows[0],
+            average1: average,
+          });
+      }
     })
-    .catch(error => handleError(error,res));
-
+    .catch((error) => handleError(error, res));
 }
 
-
-function createParkRating (yelp_id, res){
-  let SQL = `INSERT INTO parks_table (yelp_id, total_ratings, total_votes)  VALUES ($1, $2, $3 ) RETURNING *;`
-  let safequery = [yelp_id, 0 , 0]
-  client.query(SQL, safequery)
-    .then (results =>{
-      res.status(200).render('/')
+function createParkRating(yelp_id, park_name, res) {
+  let SQL = `INSERT INTO parks_table (yelp_id, park_name, total_ratings, total_votes)  VALUES ($1, $2, $3, $4) RETURNING *;`;
+  let safequery = [yelp_id, park_name, 0, 0];
+  client
+    .query(SQL, safequery)
+    .then((results) => {
+      console.log(
+        'I am results.rows+++++++++++++++++++++++++++',
+        results.rows[0]
+      );
+      let average =
+        results.rows[0].total_ratings / results.rows[0].total_votes || 0;
+      console.log('I am average 94+++++++++++++++++', average);
+      res
+        .status(200)
+        .render('pages/details', {
+          ratings: results.rows[0],
+          average1: average,
+        });
     })
-    .catch(error => handleError(error,res));
+    .catch((error) => handleError(error, res));
 }
 
-function addRatings(req,res){
-
+function addRatings(req, res) {
   //on submit of rating send users rating to database and increment # of ratings by 1
   //pull update park rating and render to page
-  
 }
 
-function comebacktome (req,res) {
+function comebacktome(req, res) {
   console.log(`req.body: ${req.body.name}`);
   let SQL = 'INSERT INTO parks (name, url) VALUES ($1, $2) RETURNING *;';
 
   let param = [req.body.name, req.body.url];
 
-  client.query(SQL, param)
-    .then(results => {
+  client
+    .query(SQL, param)
+    .then((results) => {
       console.log(`results: ${results.rows}`);
       res.redirect(`/`);
     })
-    .catch(error => {
+    .catch((error) => {
       console.log(`error with addPokemon: ${error}`);
-      res.status(500).send(error);}
-    );
+      res.status(500).send(error);
+    });
 }
 
 function handleFavorites(req, res) {
-
   //create query
   const SQL = 'SELECT * from parks';
 
   //give our SQL query to our pg 'agent'
-  client.query(SQL)
-    .then (results => {
-      res.render('favorites', {savedCritters:results.rows});
+  client
+    .query(SQL)
+    .then((results) => {
+      res.render('favorites', { savedCritters: results.rows });
       // res.status(200).json(results);
     })
-    .catch(error => {
+    .catch((error) => {
       console.log(`error with handleFavorites: ${error}`);
-      res.status(500).send(error);}
-    );
+      res.status(500).send(error);
+    });
 }
 
 function handleNotFound(req, res) {
@@ -143,7 +167,7 @@ function handleNotFound(req, res) {
 // 500 (catastrophic) error handler. Log it, and then tell the user
 function handleError(error, res) {
   console.error(error);
-  res.status(500).render('error',{error_data: error});
+  res.status(500).render('error', { error_data: error });
 }
 
 // ----------------------------------------------
@@ -172,11 +196,11 @@ function Park(obj) {
 
 //app.listen(process.env.PORT, () => console.log(`Server is running on ${process.env.PORT}`));
 
-
-client.connect()
-  .then( () => {
-    app.listen(PORT, ()=> console.log('server running on port', PORT));
+client
+  .connect()
+  .then(() => {
+    app.listen(PORT, () => console.log('server running on port', PORT));
   })
-  .catch(err => {
+  .catch((err) => {
     throw `PG startuperror: ${err.message}`;
   });
